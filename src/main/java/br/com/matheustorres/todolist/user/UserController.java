b1d3283c72b3fe524ed864d6cdd352b9529236f0
Matheus <matheustorresdev@gmail.com>
2023-10-12 13:11:10 -0300

    Implementando segurança nos dados do usuário

--- src/main/java/br/com/matheustorres/todolist/user/UserController.java
+++ src/main/java/br/com/matheustorres/todolist/user/UserController.java
@@ -8,22 +8,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    @Autowired
-    private IUserRepository userRepository;
-    
-    @PostMapping("/")
-    public ResponseEntity create(@RequestBody UserModel userModel) {
-      var user = this.userRepository.findByUsername(userModel.getUsername());
+  @Autowired
+  private IUserRepository userRepository;
 
-        if(user != null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
-        }
+  @PostMapping("/")
+  public ResponseEntity<Object> create(@RequestBody UserModel userModel) {
+    var user = this.userRepository.findByUsername(userModel.getUsername());
 
-      var userCreated =  this.userRepository.save(userModel);
-      return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    if (user != null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
     }
+
+   var passwordHashred =  BCrypt.withDefaults()
+              .hashToString(12, userModel.getPassword().toCharArray());
+
+   userModel.setPassword(passwordHashred);
+
+    var userCreated = this.userRepository.save(userModel);
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+  }
 }
